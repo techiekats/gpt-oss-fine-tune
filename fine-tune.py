5b337ea2d60006c8e3f2927af1bee460b08df1bd
KS <khyati.a.shah@gmail.com>
2025-09-05 00:50:36 -0700

    applied tokenizer

--- fine-tune.py
+++ fine-tune.py
@@ -1,8 +1,14 @@
 from huggingface_hub import notebook_login
 from datasets import load_dataset
-
+from transformers import AutoTokenizer
 
 notebook_login()
 
 dataset = load_dataset("HuggingFaceH4/Multilingual-Thinking", split="train")
+
+tokenizer = AutoTokenizer.from_pretrained("openai/gpt-oss-20b")
+
+messages = dataset[0]["messages"]
+conversation = tokenizer.apply_chat_template(messages, tokenize=False)
+print(conversation)
 
